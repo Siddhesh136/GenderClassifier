a2764463395878b0d37a2ca84f4606e64e1b191c
Siddhesh136 <siddheshkadam136@gmail.com>
2018-11-11 12:56:24 +0530

    Update gender.py

--- Desktop/data_science/gender.py
+++ Desktop/data_science/gender.py
@@ -1,17 +1,17 @@
 from sklearn import tree
 
-clf = tree.DecisionTreeClassifier()
+cls = tree.DecisionTreeClassifier()
 
 # [height, weight, shoe_size]
-X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
-     [190, 90, 47], [175, 64, 39],
-     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+X = [[181, 80, 10], [177, 70, 8], [160, 60, 7], [154, 54, 8], [166, 65, 9],
+     [190, 90, 10], [175, 64, 9],
+     [177, 70, 8], [159, 55, 7], [171, 75, 8], [181, 85, 10]]
 
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
 #training our data
-clf = clf.fit(X, Y)
-prediction = clf.predict([[160, 50, 30]])
+cls = cls.fit(X, Y)
+prediction = cls.predict([[160, 50, 30]])
 print(prediction)
 
