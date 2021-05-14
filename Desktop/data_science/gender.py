091f9a40c1a54301df678753bb8ed8a9b824c4a7
Siddhesh136 <siddheshkadam136@gmail.com>
2021-05-14 18:51:56 +0530

    Update gender.py

--- Desktop/data_science/gender.py
+++ Desktop/data_science/gender.py
@@ -3,17 +3,18 @@ from sklearn import tree
 cls = tree.DecisionTreeClassifier()
 
 # [height, weight, shoe_size]
-X = [[181, 80, 10], [177, 70, 8], [160, 60, 7], [154, 54, 8], [166, 65, 9],
-     [190, 90, 10], [175, 64, 9],
-     [177, 70, 8], [159, 55, 7], [171, 75, 8], [181, 85, 10]]
+X = [[181, 80, 10], [177, 70, 8], [160, 60, 7], [154, 54, 8], [166, 65, 9],[190, 90, 10], [175, 64, 9],
+     [177, 70, 8], [159, 55, 7], [171, 75, 8], [181, 85, 10],
+     [110, 40, 8], [190, 70, 9], [140, 45, 7], [130, 58, 6], [143, 80, 7],
+     [164, 68, 8], [180, 58, 9]]
 
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
-     'female', 'male', 'male']
+     'female', 'male', 'male','female','male','female','female','female','male','male']
 
 #training our data
 cls = cls.fit(X, Y)
 
-#Prediction
-prediction = cls.predict([[160, 50, 30]])
+#Predicting 
+prediction = cls.predict([[160, 90, 9]])
 print(prediction)
 
